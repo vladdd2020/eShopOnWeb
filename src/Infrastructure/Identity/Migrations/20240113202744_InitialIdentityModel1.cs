ba366423befacad11dd6380c521d6d799ff4169f
Paliienko Vladyslav <vlad@MacBook-Pro-Paliienko.local>
2024-01-13 22:48:04 +0200

    Added changes

---
+++ src/Infrastructure/Identity/Migrations/20240113202744_InitialIdentityModel1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microsoft.eShopWeb.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialIdentityModel1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
