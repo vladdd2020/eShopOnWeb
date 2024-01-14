79921394f49371c4d1da80cc54e81b589b0dc7bd
Paliienko Vladyslav <vlad@MacBook-Pro-Paliienko.local>
2024-01-14 09:49:20 +0200

    Added changes

--- src/Web/HealthChecks/HomePageHealthCheck.cs
+++ src/Web/HealthChecks/HomePageHealthCheck.cs
@@ -25,7 +25,7 @@ public class HomePageHealthCheck : IHealthCheck
         var client = new HttpClient();
         var response = await client.GetAsync(myUrl);
         var pageContents = await response.Content.ReadAsStringAsync();
-        if (pageContents.Contains(".NET Bot Black Sweatshirt"))
+        if (pageContents.Contains("Зарядна станція Bluetti AC200MAX"))
         {
             return HealthCheckResult.Healthy("The check indicates a healthy result.");
         }
