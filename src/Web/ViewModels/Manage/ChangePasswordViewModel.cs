05048c5dee7a187277a46f4ecea005f172c645e0
Paliienko Vladyslav <vlad@MacBook-Pro-Paliienko.local>
2024-01-14 20:33:59 +0200

    Added changes

--- src/Web/ViewModels/Manage/ChangePasswordViewModel.cs
+++ src/Web/ViewModels/Manage/ChangePasswordViewModel.cs
@@ -6,18 +6,18 @@ public class ChangePasswordViewModel
 {
     [Required]
     [DataType(DataType.Password)]
-    [Display(Name = "Current password")]
+    [Display(Name = "Поточний пароль")]
     public string? OldPassword { get; set; }
 
     [Required]
-    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [StringLength(100, ErrorMessage = "{0} повинен бути від {2} до {1} символів у довжину.", MinimumLength = 6)]
     [DataType(DataType.Password)]
-    [Display(Name = "New password")]
+    [Display(Name = "Новий пароль")]
     public string? NewPassword { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm new password")]
-    [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+    [Display(Name = "Підтвердити новий пароль")]
+    [Compare("NewPassword", ErrorMessage = "Новий пароль та підтвердження паролю не збігаються..")]
     public string? ConfirmPassword { get; set; }
 
     public string? StatusMessage { get; set; }
--- src/Web/ViewModels/Manage/SetPasswordViewModel.cs
+++ src/Web/ViewModels/Manage/SetPasswordViewModel.cs
@@ -5,14 +5,14 @@ namespace Microsoft.eShopWeb.Web.ViewModels.Manage;
 public class SetPasswordViewModel
 {
     [Required]
-    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [StringLength(100, ErrorMessage = "{0} повинен бути від {2} до {1} символів у довжину.", MinimumLength = 6)]
     [DataType(DataType.Password)]
-    [Display(Name = "New password")]
+    [Display(Name = "Новий пароль")]
     public string? NewPassword { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm new password")]
-    [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+    [Display(Name = "Підтвердити новий пароль")]
+    [Compare("NewPassword", ErrorMessage = "Новий пароль та підтвердження паролю не збігаються..")]
     public string? ConfirmPassword { get; set; }
 
     public string? StatusMessage { get; set; }
